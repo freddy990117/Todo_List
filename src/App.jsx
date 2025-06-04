b7b3800c05e85d0050c9dd23506d405fa8838d39
Freddy990117 <Freddy990117@gmail.com>
2025-06-05 07:16:16 +0800

    Add onChange &  function for user input

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useReducer, useState } from "react";
+import { useRef } from "react";
 function App() {
   // 紀錄 todo list 的資料存放狀態（ 會是一個 array ）
   const [data, setData] = useState([]);
   // 紀錄使用者輸入的狀態
-  const [input, setInput] = useState("");
+  const [inputValue, setInputValue] = useState("");
   // 存放 React 唯一值 （畫面 Render 也不改變）
-  // const idRef = useRef(0);
+  const idRef = useRef(0);
+
+  // 設定 新增 Todo List 的 function
+  const addFun = () => {
+    // 如果 inputValue 是空值的話，返回空值
+    if (inputValue === "") return "";
+
+    const todoData = {
+      // id 設定不會改變的 useRef 值
+      id: idRef.current++,
+      // 值是使用者輸入的資訊
+      text: inputValue,
+      // 是否完成
+      isComplete: false,
+    };
+
+    setData([...data, todoData]);
+  };
+
   return (
     <section className="container">
       {/* Title 的文字（Todo Application） */}
@@ -18,10 +37,10 @@ function App() {
       {/* Card Components 預計會在這邊更換 */}
       <section className="main">
         <div className="card">
-          <div className="card-text">i will wake up on 5 AM</div>
+          {/* <div className="card-text">i will wake up on 5 AM</div>
           <button className="remove">
             <FontAwesomeIcon icon={faTrash} />
-          </button>
+          </button> */}
         </div>
       </section>
       {/* 輸入與新增的按鈕 */}
@@ -30,6 +49,10 @@ function App() {
           type="text"
           placeholder="Add somthing......."
           className="todo-input"
+          // 綁定 value 的值是使用者輸入的值
+          value={inputValue}
+          // 設定監聽事件，追蹤 inputValue
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button className="add-btn">Add Todo</button>
       </section>

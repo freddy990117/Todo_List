b2c2eb03296a5bea77a94a324c26bb93ea840f84
Freddy <freddy990117@gmail.com>
2025-10-12 11:43:46 +0800

    1.Add userEvent to test user Event 2.Add Test for input & click form

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,21 +2,39 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom"; // 加入這行才能用 toBeInTheDocument
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 // 測試進入時是否有顯示內文「Todo Application 與 input 中的 placeholder」
-describe("進入時是否有顯示文字", () => {
-  // 測試之前執行 (beforeEach)
+// describe("進入時是否有顯示文字", () => {
+//   // 測試之前執行 (beforeEach)
+//   beforeEach(() => {
+//     render(<App />);
+//   });
+//   test("1.是否有顯示標題 Todo Application (title)", () => {
+//     const titleFont = screen.getByText("Todo Application");
+
+//     expect(titleFont).toBeInTheDocument();
+//   });
+//   test("2.進入時是否有顯示 Add Somthing...(placeholder)", () => {
+//     const placeholderText = screen.getByPlaceholderText("Add somthing.......");
+
+//     expect(placeholderText).toBeInTheDocument();
+//   });
+// });
+
+describe("測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
   beforeEach(() => {
     render(<App />);
   });
-  test("1.是否有顯示標題 Todo Application (title)", () => {
-    const titleFont = screen.getByText("Todo Application");
+  test("輸入表單", async () => {
+    const inputText = screen.getByPlaceholderText("Add somthing.......");
+    const addbtn = screen.getByText("Add Todo");
 
-    expect(titleFont).toBeInTheDocument();
-  });
-  test("2.進入時是否有顯示 Add Somthing...(placeholder)", () => {
-    const placeholderText = screen.getByPlaceholderText("Add somthing.......");
+    // 模擬使用者輸入的行為
+    await userEvent.type(inputText, "今天記得刷載具");
+    // 模擬使用者點擊按鈕的行為
+    await userEvent.click(addbtn);
 
-    expect(placeholderText).toBeInTheDocument();
+    expect(screen.getByText("今天記得刷載具")).toBeInTheDocument();
   });
 });

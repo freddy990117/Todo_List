85bb10fa373bf001b93e0710607fdb2e656f0825
Freddy990117 <Freddy990117@gmail.com>
2025-10-14 07:18:10 +0800

    Add test : Click Edit, enter text, and press Enter to save the latest information.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,24 +5,24 @@ import App from "./App";
 import userEvent from "@testing-library/user-event";
 
 // 測試進入時是否有顯示內文「Todo Application 與 input 中的 placeholder」
-// describe("進入時是否有顯示文字", () => {
-//   // 測試之前執行 (beforeEach)
-//   beforeEach(() => {
-//     render(<App />);
-//   });
-//   test("1.是否有顯示標題 Todo Application (title)", () => {
-//     const titleFont = screen.getByText("Todo Application");
+describe("1.測試進入時的顯示：", () => {
+  // 測試之前執行 (beforeEach)
+  beforeEach(() => {
+    render(<App />);
+  });
+  test("1-1.進入時是否有顯示標題 Todo Application (title)", () => {
+    const titleFont = screen.getByText("Todo Application");
 
-//     expect(titleFont).toBeInTheDocument();
-//   });
-//   test("2.進入時是否有顯示 Add Somthing...(placeholder)", () => {
-//     const placeholderText = screen.getByPlaceholderText("Add somthing.......");
+    expect(titleFont).toBeInTheDocument();
+  });
+  test("2-1.進入時是否有顯示 Add Somthing...(placeholder)", () => {
+    const placeholderText = screen.getByPlaceholderText("Add somthing.......");
 
-//     expect(placeholderText).toBeInTheDocument();
-//   });
-// });
+    expect(placeholderText).toBeInTheDocument();
+  });
+});
 
-describe("1.測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
+describe("2.測試輸入表單後的行為：", () => {
   beforeEach(async () => {
     render(<App />);
     const inputText = screen.getByPlaceholderText("Add somthing.......");
@@ -33,11 +33,11 @@ describe("1.測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
     // 模擬使用者點擊按鈕的行為
     await userEvent.click(addbtn);
   });
-  test("輸入表單", async () => {
+  test("2-1.輸入後是否有出現表單", async () => {
     //確認是否有輸入並顯示在畫面上
     expect(screen.getByText("今天記得刷載具")).toBeInTheDocument();
   });
-  test("表單輸入後，是否有「更多」「編輯」「移除」「完成」的選項", async () => {
+  test("2-2.表單輸入後，是否有「更多」「編輯」「移除」「完成」的選項", async () => {
     // 先抓「更多」的選項
     const EllipsisBtn = screen.getByLabelText("Ellipsis");
     // 按下「更多」的選項後再抓取其他的元素
@@ -50,5 +50,23 @@ describe("1.測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
     expect(editbtn).toBeVisible();
     expect(removebtn).toBeVisible();
     expect(checkbtn).toBeVisible();
+  });
+  test("2-3.點擊編輯後輸入文字按下 Enter 是否會儲存最新的資訊", async () => {
+    // 先抓「更多」的選項
+    const EllipsisBtn = screen.getByLabelText("Ellipsis");
+    // 按下「更多」的選項後再抓取其他的元素
+    await userEvent.click(EllipsisBtn);
+    const editbtn = screen.getByLabelText("edit");
+    // 點擊編輯的選項
+    await userEvent.click(editbtn);
+    // 是否有出現 "按下 Enter 自動儲存....."
+    const editInput = screen.getByPlaceholderText("按下 Enter 自動儲存.....");
+    expect(editInput);
+
+    // 使用者輸入
+    await userEvent.type(editInput, "今天要去健身");
+    // 使用者按下 Enter
+    await userEvent.keyboard("{Enter}");
+    expect(screen.getByText("今天要去健身")).toBeInTheDocument();
   });
 });

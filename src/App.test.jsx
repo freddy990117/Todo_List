b7dff78db778582d144ea3c8d4b91ffa430a45f8
Freddy <freddy990117@gmail.com>
2025-10-24 09:31:11 +0800

    Add test for Edit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,53 +5,53 @@ import App from "./App";
 import userEvent from "@testing-library/user-event";
 
 // 測試進入時是否有顯示內文「Todo Application 與 input 中的 placeholder」
-// describe("1.進入時是否有顯示文字", () => {
-//   // 測試之前執行 (beforeEach)
-//   beforeEach(() => {
-//     render(<App />);
-//   });
-//   test("1-1.是否有顯示標題 Todo Application (title)", () => {
-//     const titleFont = screen.getByText("Todo Application");
+describe("1.進入時是否有顯示文字", () => {
+  // 測試之前執行 (beforeEach)
+  beforeEach(() => {
+    render(<App />);
+  });
+  test("1-1.是否有顯示標題 Todo Application (title)", () => {
+    const titleFont = screen.getByText("Todo Application");
 
-//     expect(titleFont).toBeInTheDocument();
-//   });
-//   test("1-2.進入時是否有顯示 Add Somthing...(placeholder)", () => {
-//     const placeholderText = screen.getByPlaceholderText("Add somthing.......");
+    expect(titleFont).toBeInTheDocument();
+  });
+  test("1-2.進入時是否有顯示 Add Somthing...(placeholder)", () => {
+    const placeholderText = screen.getByPlaceholderText("Add somthing.......");
 
-//     expect(placeholderText).toBeInTheDocument();
-//   });
-// });
+    expect(placeholderText).toBeInTheDocument();
+  });
+});
 
-// describe("2.測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
-//   beforeEach(async () => {
-//     render(<App />);
-//     const inputText = screen.getByPlaceholderText("Add somthing.......");
-//     const addbtn = screen.getByText("Add Todo");
+describe("2.測試輸入 input 後點擊 Add btn 是否有出現表單", () => {
+  beforeEach(async () => {
+    render(<App />);
+    const inputText = screen.getByPlaceholderText("Add somthing.......");
+    const addbtn = screen.getByText("Add Todo");
 
-//     // 模擬使用者輸入的行為
-//     await userEvent.type(inputText, "今天記得刷載具");
-//     // 模擬使用者點擊按鈕的行為
-//     await userEvent.click(addbtn);
-//   });
-//   test("2-1輸入表單", async () => {
-//     //確認是否有輸入並顯示在畫面上
-//     expect(screen.getByText("今天記得刷載具")).toBeInTheDocument();
-//   });
-//   test("2-2表單輸入後，是否有「更多」「編輯」「移除」「完成」的選項", async () => {
-//     // 先抓「更多」的選項
-//     const EllipsisBtn = screen.getByLabelText("Ellipsis");
-//     // 按下「更多」的選項後再抓取其他的元素
-//     await userEvent.click(EllipsisBtn);
+    // 模擬使用者輸入的行為
+    await userEvent.type(inputText, "今天記得刷載具");
+    // 模擬使用者點擊按鈕的行為
+    await userEvent.click(addbtn);
+  });
+  test("2-1輸入表單", async () => {
+    //確認是否有輸入並顯示在畫面上
+    expect(screen.getByText("今天記得刷載具")).toBeInTheDocument();
+  });
+  test("2-2表單輸入後，是否有「更多」「編輯」「移除」「完成」的選項", async () => {
+    // 先抓「更多」的選項
+    const EllipsisBtn = screen.getByLabelText("Ellipsis");
+    // 按下「更多」的選項後再抓取其他的元素
+    await userEvent.click(EllipsisBtn);
 
-//     const editbtn = screen.getByLabelText("edit");
-//     const removebtn = screen.getByLabelText("remove");
-//     const checkbtn = screen.getByLabelText("check");
-//     // 是否有顯示在畫面上
-//     expect(editbtn).toBeVisible();
-//     expect(removebtn).toBeVisible();
-//     expect(checkbtn).toBeVisible();
-//   });
-// });
+    const editbtn = screen.getByLabelText("edit");
+    const removebtn = screen.getByLabelText("remove");
+    const checkbtn = screen.getByLabelText("check");
+    // 是否有顯示在畫面上
+    expect(editbtn).toBeVisible();
+    expect(removebtn).toBeVisible();
+    expect(checkbtn).toBeVisible();
+  });
+});
 
 describe("3.測試表單開啟之後的行為", () => {
   beforeEach(async () => {
@@ -66,12 +66,8 @@ describe("3.測試表單開啟之後的行為", () => {
     const EllipsisBtn = screen.getByLabelText("Ellipsis");
     // 按下「更多」的選項後再抓取其他的元素
     await userEvent.click(EllipsisBtn);
-
-    // const removebtn = screen.getByLabelText("remove");
-    // const checkbtn = screen.getByLabelText("check");
-    // const editbtn = screen.getByLabelText("edit");
   });
-  test("3-1.點選刪除後是否不在文件內", async () => {
+  test("3-1.點選刪除後是否不在 DOM 中", async () => {
     const removebtn = screen.getByLabelText("remove");
     // 模擬使用者點擊刪除的行為
     await userEvent.click(removebtn);
@@ -86,8 +82,19 @@ describe("3.測試表單開啟之後的行為", () => {
     await userEvent.click(checkbtn);
     // 取得該 DOM 元素
     const todoText = screen.getByText("今天記得刷載具");
-    // COM 元素中是否有出現該 check 元素
+    // DOM 元素中是否有出現該 check 元素
     expect(todoText).toHaveClass("check");
   });
-  // test("3-3.點選編輯並儲存後文件是否有更新", async () => {});
+  test("3-3.確認編輯後儲存是否有更新", async () => {
+    const editbtn = screen.getByLabelText("edit");
+    // 模擬使用者點擊編輯的行為
+    await userEvent.click(editbtn);
+    // 取得輸入框框
+    const inputText = screen.getByPlaceholderText("按下 Enter 自動儲存.....");
+    // 模擬使用者輸入表單並按下 Enter
+    await userEvent.type(inputText, "今天已經刷過載具了");
+    await userEvent.keyboard("{enter}");
+    // DOM 元素中是否有更改後的文字
+    expect(screen.getByText("今天已經刷過載具了"));
+  });
 });
